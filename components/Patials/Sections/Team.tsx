a0a6462651ceb9f50e5d765b3705ce06140bbc6e
waterburner <oleg.kovelsky.job@gmail.com>
2022-03-11 17:25:48 -0600

    minor adjustment on team bios section

--- components/Patials/Sections/Team.tsx
+++ components/Patials/Sections/Team.tsx
@@ -20,15 +20,17 @@ export const TeamSection = () => {
         </Heading>
       </AnimateWrapper>
       <div className="team-container mt-[25px] tablet:mt-[30px] xl:mt-[52px]">
-        <div className="grid grid-cols-2 gap-y-10 tablet:gap-y-[60px] xl:gap-y-[89px] gap-5 tablet:gap-x-[82px] xl:gap-x-[133px] text-center">
+        <div className="grid grid-cols-2 gap-y-10 tablet:gap-y-[60px] xl:gap-y-[89px] gap-5 tablet:gap-x-[82px] text-center">
           {teamData.map((team, index) => (
             <div key={index}>
               <AnimateWrapper yOffset={70} delayOrder={2 + index * 0.5} animateType={AnimateType.FADEIN}>
-                <img
-                  src={team.profileImg}
-                  className="max-w-full rounded-[20px]"
-                  alt=""
-                />
+                <div className="xl:px-4">
+                  <img
+                    src={team.profileImg}
+                    className="max-w-full rounded-[20px]"
+                    alt=""
+                  />
+                </div>
                 <Paragraph className="font-bold text-white mt-[18px] xl:mt-7 text-3.75 tablet:text-5 xl:text-content">
                   {team.name}
                 </Paragraph>
