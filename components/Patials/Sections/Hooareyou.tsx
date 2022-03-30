22f352d7d9edb5d15d5b5ccaefbe7066c92b61ba
waterburner <oleg.kovelsky.job@gmail.com>
2022-03-30 17:13:46 -0500

    adding animation for body text

--- components/Patials/Sections/Hooareyou.tsx
+++ components/Patials/Sections/Hooareyou.tsx
@@ -8,36 +8,52 @@ import { HooLeft, HooRight } from '../../Elements/Icons/Icons'
 export const Hooareyou = () => {
   return (
     <Section
-      className="mx-auto max-w-[500px] tablet:max-w-[900px] xl:max-w-[1298px] px-[10px] tablet:px-10 pt-14 tablet:pt-[140px]"
+      className="mx-auto max-w-[500px] px-[10px] pt-14 tablet:max-w-[900px] tablet:px-10 tablet:pt-[140px] xl:max-w-[1298px]"
       navigateID="topSection"
     >
-      <AnimateWrapper xOffset={-100} delayOrder={1} animateType={AnimateType.SLIDEIN}>
+      <AnimateWrapper
+        xOffset={-100}
+        delayOrder={1}
+        animateType={AnimateType.SLIDEIN}
+      >
         <Heading className="text-center text-white text-headingMobile tablet:text-8 xl:text-heading">
           Hoo Are You?
         </Heading>
       </AnimateWrapper>
-      <div className="mt-[18px] tablet:mt-[31px] text-center flex items-center flex-col tablet:flex-row relative">
-        <div className="absolute hidden tablet:block left-0 max-w-[170px]">
-          <HooLeft width="100%" />
+      <AnimateWrapper
+        xOffset={-100}
+        delayOrder={1.5}
+        animateType={AnimateType.SLIDEIN}
+      >
+        <div className="relative mt-[18px] flex flex-col items-center text-center tablet:mt-[31px] tablet:flex-row">
+          <div className="absolute left-0 hidden max-w-[170px] tablet:block">
+            <HooLeft width="100%" />
+          </div>
+          <div className="mx-auto">
+            <Paragraph className="mt-1 leading-6 font-RedHat text-3 text-primary tablet:mt-2 tablet:text-4 xl:mt-3 xl:text-headingMobile">
+              <span className="font-RedHatBold">Talented artist</span> who feels
+              like your time is being wasted at a day job.
+            </Paragraph>
+            <Paragraph className="mt-1 leading-6 font-RedHat text-3 text-primary tablet:mt-2 tablet:text-4 xl:mt-3 xl:text-headingMobile">
+              <span className="font-RedHatBold">NFT investor</span> who wants
+              access to the hottest new projects.
+            </Paragraph>
+            <Paragraph className="mt-1 leading-6 font-RedHat text-3 text-primary tablet:mt-2 tablet:text-4 xl:mt-3 xl:text-headingMobile">
+              <span className="font-RedHatBold">Fan of art and music</span> who
+              wants to get close with the creatives.
+            </Paragraph>
+            <Paragraph className="mt-1 leading-6 font-RedHat text-3 text-primary tablet:mt-2 tablet:text-4 xl:mt-3 xl:text-headingMobile">
+              <span className="font-RedHatBold">
+                Aspiring artist or musician
+              </span>{' '}
+              looking to improve your craft.
+            </Paragraph>
+          </div>
+          <div className="absolute right-0 hidden max-w-[170px] tablet:block">
+            <HooRight width="100%" />
+          </div>
         </div>
-        <div className="mx-auto">
-          <Paragraph className="mt-1 tablet:mt-2 xl:mt-3 text-3 leading-6 tablet:text-4 xl:text-headingMobile font-RedHat text-primary">
-            <span className="font-RedHatBold">Talented artist</span> who feels like your time is being wasted at a day job.
-          </Paragraph>
-          <Paragraph className="mt-1 tablet:mt-2 xl:mt-3 text-3 leading-6 tablet:text-4 xl:text-headingMobile font-RedHat text-primary">
-            <span className="font-RedHatBold">NFT investor</span> who wants access to the hottest new projects.
-          </Paragraph>
-          <Paragraph className="mt-1 tablet:mt-2 xl:mt-3 text-3 leading-6 tablet:text-4 xl:text-headingMobile font-RedHat text-primary">
-            <span className="font-RedHatBold">Fan of art and music</span> who wants to get close with the creatives.
-          </Paragraph>
-          <Paragraph className="mt-1 tablet:mt-2 xl:mt-3 text-3 leading-6 tablet:text-4 xl:text-headingMobile font-RedHat text-primary">
-            <span className="font-RedHatBold">Aspiring artist or musician</span> looking to improve your craft.
-          </Paragraph>
-        </div>
-        <div className="absolute hidden tablet:block right-0 max-w-[170px]">
-          <HooRight width="100%" />
-        </div>
-      </div>
+      </AnimateWrapper>
     </Section>
   )
 }
