852fac650e85dad84320fa28add02ae560942599
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-07-12 16:38:59 -0500

    feat(walletConnect): added connect btn

---
+++ components/Elements/Button/Web3Button.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useWeb3 } from '../../../hooks'
+import Button from './index'
+import { ButtonSize } from '../../../types/types'
+
+interface ConnectProps {
+  connect: (() => Promise<void>) | null
+}
+const ConnectButton = ({ connect }: ConnectProps) => {
+  return connect ? (
+    <Button size={ButtonSize.SMALL} className="" onClick={connect}>
+
+      Connect
+  </Button>
+
+    // <button onClick={connect}>Connect</button>
+  ) : (
+    <button>Loading...</button>
+  )
+}
+
+interface DisconnectProps {
+  disconnect: (() => Promise<void>) | null
+}
+
+const DisconnectButton = ({ disconnect }: DisconnectProps) => {
+  return disconnect ? (
+    <Button size={ButtonSize.SMALL} className="" onClick={disconnect}>
+      Disconnect
+    </Button>
+
+    // <button onClick={disconnect}>Disconnect</button>
+  ) : (
+    <button>Loading...</button>
+  )
+}
+
+export function Web3Button() {
+  const { web3Provider, connect, disconnect } = useWeb3()
+
+  return web3Provider ? (
+    <DisconnectButton disconnect={disconnect} />
+  ) : (
+    <ConnectButton connect={connect} />
+  )
+}
