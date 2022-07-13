1efe6096f5f01f2ad704d46ef2fb8e2fba4659c6
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-07-13 16:38:18 -0500

    feat(client): added more stuff

--- components/Elements/Button/MintButton.tsx
+++ components/Elements/Button/MintButton.tsx
@@ -18,8 +18,26 @@ const PublicMintButton = ({ mint }: PublicProps) => {
     <button>Loading...</button>
   )
 }
+interface PublicProps {
+  mint: (() => Promise<void>) | null
+}
+const WhiteListMintButton = ({ mint }: PublicProps) => {
+  return mint ? (
+    <Button
+      size={ButtonSize.PRIMARY}
+      className="mx-auto mt-[30px] tablet:mt-7 xl:mt-[34px]"
+      onClick={mint}>
+      Mint
+    </Button>
+  ) : (
+    <button>Loading...</button>
+  )
+}
 
 export function MintButton() {
-  const { web3Provider, publicMint } = useWeb3()
-  return ( <PublicMintButton mint={publicMint} />)
+  const { web3Provider, publicMint,whiteListMint } = useWeb3()
+  let isPublicMint=false
+  return isPublicMint ? 
+    ( <PublicMintButton mint={publicMint} />) :
+    ( <WhiteListMintButton mint={publicMint} />) 
 }
