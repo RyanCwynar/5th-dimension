1e6a713f21a1b8b5ed5b6db4e2f237f110e05f02
sdelvalle57 <sdelvalle57@gmail.com>
2022-07-15 09:11:22 -0500

    set linear minting and implement ownable

--- contracts/FifthDimension.sol
+++ contracts/FifthDimension.sol
@@ -7,32 +7,24 @@ https://fueledonbacon.com
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FifthDimension is ERC721, AccessControl {
-    bytes32 public ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+
+contract FifthDimension is ERC721, Ownable {
     using Strings for uint256;
+    using Counters for Counters.Counter;
 
-    bytes32 private _whitelistMerkleRoot;
-
-    mapping(address => uint256) private _whitelistClaimed;
-    mapping(address => uint256) private _publicSaleClaimed;
-
-    string private _baseUri;
-    string private _tempUri;
+    uint256 private constant _COLLECTION_SIZE = 555;
+    uint256 private constant _AIRDROP_LIMIT = 55;
+    uint256 private constant _WHITELIST_LIMIT = 1;
+    uint256 private constant _PUBLIC_LIMIT = 2;
     
     uint256 public revealed;
     uint256 private _overridePrivateSale;
     uint256 private _overridePublicSale;
-
-    uint256 private constant _WHITELIST_LIMIT = 1;
-    uint256 private constant _PUBLIC_LIMIT = 2;
-
-    uint256[500] private _communityIds;
-    uint256[55] private _teamIds;
-    uint256 private _communityIndex;
-    uint256 private _teamIndex;
 
     uint256 private _supplyTeamWallet; //pairs with _AIRDROP_LIMIT
     uint256 private _supplyCommunity; //pairs with _COLLECTION_SIZE - _AIRDROP_LIMIT
@@ -41,6 +33,16 @@ contract FifthDimension is ERC721, AccessControl {
     uint256 public whitelistEnd;
     uint256 public publicStart;
     uint256 public publicEnd;
+
+    string private _baseUri;
+    string private _tempUri;
+
+    bytes32 private _whitelistMerkleRoot;
+
+    Counters.Counter private _tokenIds;
+
+    mapping(address => uint256) private _whitelistClaimed;
+    mapping(address => uint256) private _publicSaleClaimed;
 
     constructor(
         uint256 _whitelistStart,
@@ -65,22 +67,16 @@ contract FifthDimension is ERC721, AccessControl {
         require(_publicEnd > timestamp && _publicEnd > _publicStart, "Wrong public end");
         publicStart = _publicStart; 
         publicEnd = _publicEnd; 
-        address account = _msgSender();
-        _setupRole(DEFAULT_ADMIN_ROLE, account);
-        _setupRole(ADMIN_ROLE, account);
-    }
-
-    modifier onlyAdmin(){
-        address account = _msgSender();
-        require(hasRole(DEFAULT_ADMIN_ROLE, account) || hasRole(ADMIN_ROLE, account) , "Must be an admin");
-        _;
     }
 
     /// @notice send more gas if you are minting a high quantity.
-    function airdrop(address to, uint16 quantity) external onlyAdmin {
+    function airdrop(address to, uint16 quantity) external onlyOwner {
         _supplyTeamWallet += quantity;
-        for(uint16 i = 0; i < quantity; i++) {
-            _safeMint(to, _pickRandomTeamUniqueId());
+        require(_supplyTeamWallet <= _AIRDROP_LIMIT, "REACHED_AIRDROP_LIMIT");
+        for(uint index = 0; index < quantity; index++) {
+            _tokenIds.increment();
+            uint id = _tokenIds.current();
+            _safeMint(to, id);
         }
     }
 
@@ -95,30 +91,26 @@ contract FifthDimension is ERC721, AccessControl {
     /// @notice overrides public and private sale
     /// @param publicSale true if override public sale
     /// @param privateSale true if override private sale
-    function overrideSales(bool publicSale, bool privateSale) external onlyAdmin {
+    function overrideSales(bool publicSale, bool privateSale) external onlyOwner {
         _overridePublicSale = publicSale ? 1 : 0;
         _overridePrivateSale = privateSale ? 1: 0;
     }
 
-    function toggleReveal() external onlyAdmin {
+    function toggleReveal() external onlyOwner {
         revealed = revealed == 0 ? 1 : 0;
     }
 
-    function setBaseURI(string memory baseUri) external onlyAdmin {
+    function setBaseURI(string memory baseUri) external onlyOwner {
         _baseUri = baseUri;
     }
 
-    function setPlaceholderURI(string memory tempUri) external onlyAdmin {
+    function setPlaceholderURI(string memory tempUri) external onlyOwner {
         _tempUri = tempUri;
     }
 
     /// @dev override base uri. It will be combined with token ID
     function _baseURI() internal view override returns (string memory) {
         return _baseUri;
-    }
-
-    function _verifyList(bytes32[] calldata _merkleProof, bytes32 root, address addr) private pure returns(bool) {
-        return (MerkleProof.verify(_merkleProof, root, keccak256(abi.encodePacked(addr))) == true);
     }
 
     function _verifyWhitelist(bytes32[] calldata _merkleProof, address addr) private view returns(bool) {
@@ -132,8 +124,7 @@ contract FifthDimension is ERC721, AccessControl {
         require(_verifyWhitelist(_merkleProof, account), "PRESALE_NOT_VERIFIED");
         require(_whitelistClaimed[account] == 0, "WHITELIST_TOKEN_CLAIMED");
         _whitelistClaimed[account] = 1;
-        _supplyCommunity += 1;
-        _safeMint(account, _pickRandomCommunityUniqueId());
+        _mint(account);
     }
 
     /// @notice may mint up to 2 tokens per transaction at the public sale price.
@@ -142,8 +133,16 @@ contract FifthDimension is ERC721, AccessControl {
         address account = _msgSender();
         _publicSaleClaimed[account] += 1;
         require(_publicSaleClaimed[account] <= _PUBLIC_LIMIT, "PUBLIC_TOKEN_LIMIT");
+        _mint(account);
+    }
+
+    function _mint(address to) private {
+        require(_supplyTeamWallet >= _AIRDROP_LIMIT, "AIRDROP_NOT_COMPLETED");
         _supplyCommunity += 1;
-        _safeMint(_msgSender(), _pickRandomCommunityUniqueId());
+        require(_supplyCommunity <= _COLLECTION_SIZE - _AIRDROP_LIMIT, "REACHED_MAX_SUPPLY");
+        _tokenIds.increment();
+        uint id = _tokenIds.current();
+        _safeMint(to, id);
     }
 
     function tokenURI(uint256 id) public view override returns (string memory) {
@@ -154,37 +153,13 @@ contract FifthDimension is ERC721, AccessControl {
             : _tempUri;
     }
 
-    function _pickRandomCommunityUniqueId() private returns (uint256 id) {
-        uint256 random = uint256(keccak256(abi.encodePacked(_communityIndex, msg.sender, block.timestamp, blockhash(block.number-1))));
-        uint256 len = _communityIds.length - _communityIndex;
-        _communityIndex += 1;
-
-        require(len > 0, 'no _communityIds left');
-        uint256 randomIndex = random % len;
-        id = _communityIds[randomIndex] != 0 ? _communityIds[randomIndex] : randomIndex;
-        id += 1 + 55;
-        _communityIds[randomIndex] = _communityIds[len - 1] == 0 ? len - 1 : _communityIds[len - 1];
-        _communityIds[len - 1] = 0;
-    }
-
-
-    function _pickRandomTeamUniqueId() private returns (uint256 id) {
-        uint256 random = uint256(keccak256(abi.encodePacked(_teamIndex, msg.sender, block.timestamp, blockhash(block.number-1))));
-        uint256 len = _teamIds.length - _teamIndex;
-        _teamIndex += 1;
-        require(len > 0, 'no _teamIds left');
-        uint256 randomIndex = random % len;
-        id = _teamIds[randomIndex] != 0 ? _teamIds[randomIndex] : randomIndex;
-        id += 1;
-        _teamIds[randomIndex] = _teamIds[len - 1] == 0 ? len - 1 : _teamIds[len - 1];
-        _teamIds[len - 1] = 0;
-    }
+   
 
     function totalSupply() external view returns(uint256) {
         return _supplyTeamWallet + _supplyCommunity;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
--- test/nft.js
+++ test/nft.js
@@ -22,147 +22,72 @@ describe('5th Dimension', function () {
   let merkleRoot;
 
 
-  before(async function() {
+  before(async function () {
     signers = await ethers.getSigners();
 
-    
+
   });
 
-  beforeEach(async function() {
+  beforeEach(async function () {
 
     timestamp = (await getLatestTimestamp()).toNumber()
-    whitelistStart = timestamp + 60*60 //starts in 60 minutes
-    whitelistEnd = whitelistStart + 60*5; //lasts 5 minutes
+    whitelistStart = timestamp + 60 * 60 //starts in 60 minutes
+    whitelistEnd = whitelistStart + 60 * 5; //lasts 5 minutes
     publicStart = whitelistEnd + 60; //one minute after whitelisEnd
-    publicEnd = publicStart + 60*5; //lasts 5 minutes
+    publicEnd = publicStart + 60 * 5; //lasts 5 minutes
     merkleRoot = merkleTree(signers);
     timestamp = (await getLatestTimestamp()).toNumber()
 
+    console.log(timestamp)
 
-    const NFT = await ethers.getContractFactory("FifthDimensionMock");
+
+    const NFT = await ethers.getContractFactory("FifthDimension");
 
     nft = await NFT.deploy(
-        whitelistStart,
-        whitelistEnd,
-        publicStart,
-        publicEnd,
-        merkleRoot.getHexRoot(),
-        "5th Dimension",
-        "5TH",
-        "www.tempuri.com/"
+      whitelistStart,
+      whitelistEnd,
+      publicStart,
+      publicEnd,
+      merkleRoot.getHexRoot(),
+      "5th Dimension",
+      "5TH",
+      "www.tempuri.com/"
     );
     await nft.deployed()
   });
 
-
-  it('Test team randomness', async function () {
-
-    let teamArray = []
-    await Promise.all((new Array(55).fill(0)).map(async a => {
-        try {
-            let tx = await nft.pickRandomTeamUniqueId({ gasLimit: "100000"});
-            tx = await tx.wait()
-            const tokenId = tx.events[0].args[0].toNumber();
-            expect(teamArray).not.to.be.containing(tokenId);
-            teamArray.push(tokenId)
-        } catch(e) {
-            console.log(e)
-        }
-    }))
-    expect(teamArray.length).to.be.equal(55)
-    expect(teamArray).not.to.be.containing(56);
-    teamArray = teamArray.sort((a, b) => a-b);
-    expect(teamArray[0]).to.be.equal(1)
-    expect(teamArray[54]).to.be.equal(55)
-
-    //tries to pick another random
-    await expect(
-        nft.pickRandomTeamUniqueId({ gasLimit: "100000"})
-    ).to.be.revertedWith('no _teamIds left');
-
-
-  }).timeout(100000);
-
-  it('Test community randomness', async function () {
-
-    let communityArray = []
-    await Promise.all((new Array(500).fill(0)).map(async a => {
-        try {
-            let tx = await nft.pickRandomCommunityUniqueId({ gasLimit: "100000"});
-            tx = await tx.wait()
-            const tokenId = tx.events[0].args[0].toNumber();
-            expect(communityArray).not.to.be.containing(tokenId);
-            communityArray.push(tokenId)
-        } catch(e) {
-            console.log(e)
-        }
-        
-    }))
-    expect(communityArray.length).to.be.equal(500)
-    expect(communityArray).not.to.be.containing(55);
-    communityArray = communityArray.sort((a, b) => a-b);
-    expect(communityArray[0]).to.be.equal(56)
-    expect(communityArray[499]).to.be.equal(555)
-
-    //tries to pick another random
-    await expect(
-        nft.pickRandomCommunityUniqueId({ gasLimit: "100000"})
-    ).to.be.revertedWith('no _communityIds left');
-
-  }).timeout(100000);
-
   it('Test airdrop', async function () {
 
-    let minted = []
-    //airdrop 5 for 5 team members = 25
-    await Promise.all(new Array(5).fill(0).map(async (a, i) => {
-        let tx = await nft.airdrop(signers[i+1].address, 5, {gasLimit: "400000"});
-        tx = await tx.wait()
-        const tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
-        expect(tokens.length).to.be.equal(5)
-        minted = [...minted, ...tokens]
-        if(i == 0) console.log("airdrop 5 tokens", tx.gasUsed.toNumber())
-    }))
-    expect(minted.length).to.be.equal(25)
-
-    //airdrop 30 for marketing
-    let tx = await nft.airdrop(signers[9].address, 30, {gasLimit: "1400000"});
+    //airdrop 55 for team
+    let tx = await nft.airdrop(signers[9].address, 55, { gasLimit: "7000000" });
     tx = await tx.wait()
-    console.log("airdrop 30 tokens", tx.gasUsed.toNumber())
-    const tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
-    expect(tokens.length).to.be.equal(30)
-    minted = [...minted, ...tokens]
-    console.log("Gas Used to airdrop 30 tokens", tx.gasUsed.toNumber())
-
-    const supply = await nft.totalSupply()
-    expect(supply).to.be.equal(55);
-    console.log("Airdrop tokens minted");
-
-    const orderedArray = [];
-    for(let j  = 1 ; j <= 55; j++) {
-        orderedArray.push(j)
-    }
-    expect(minted.sort((a, b) => a -b)).to.be.equalTo(orderedArray);    
+    console.log("airdrop 55 tokens", tx.gasUsed.toNumber())
 
     //tries to airdrop more
     await expect(
-        nft.airdrop(signers[8].address, 1)
-    ).to.be.revertedWith('no _teamIds left');
-    
+      nft.airdrop(signers[8].address, 1)
+    ).to.be.revertedWith('REACHED_AIRDROP_LIMIT');
   })
 
-  it("Test whitelist mint", async function() {
+  it("Test whitelist mint", async function () {
     //tries to mint where whitelist no started
     await expect(
-        nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
+      nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
     ).to.be.revertedWith('PRESALE_INACTIVE');
 
-    await timeIncreaseTo(timestamp + 60*61)
+    await timeIncreaseTo(timestamp + 60 * 61)
 
     //tries to mint with wrong merkleroot
     await expect(
-        nft.connect(signers[2]).whitelistMint(getProof(merkleRoot, signers[1].address))
+      nft.connect(signers[2]).whitelistMint(getProof(merkleRoot, signers[1].address))
     ).to.be.revertedWith('PRESALE_NOT_VERIFIED');
+
+    //tries to mint where airdrop not finalized
+    await expect(
+      nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
+    ).to.be.revertedWith('AIRDROP_NOT_COMPLETED');
+
+    await nft.airdrop(signers[9].address, 55, { gasLimit: "7000000" });
 
     let minted = []
     let tx = await nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
@@ -192,20 +117,20 @@ describe('5th Dimension', function () {
     minted = [...minted, ...tokens]
     expect(minted.length).to.be.equal(6);
     minted.map(m => {
-        expect(m).to.be.greaterThan(55)
+      expect(m).to.be.greaterThan(55)
     })
-    expect(await nft.totalSupply()).to.be.equal(6)
+    expect(await nft.totalSupply()).to.be.equal(55+6)
 
-     //tries to claim again
-     await expect(
-        nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
+    //tries to claim again
+    await expect(
+      nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
     ).to.be.revertedWith('WHITELIST_TOKEN_CLAIMED');
 
-    await timeIncreaseTo(whitelistStart + 60*6)
+    await timeIncreaseTo(whitelistStart + 60 * 6)
 
     //tries to mint where whitelist ended
     await expect(
-        nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
+      nft.connect(signers[1]).whitelistMint(getProof(merkleRoot, signers[1].address))
     ).to.be.revertedWith('PRESALE_INACTIVE');
 
     //override private sale
@@ -215,100 +140,102 @@ describe('5th Dimension', function () {
     minted = [...minted, ...tokens]
     expect(minted.length).to.be.equal(7);
     minted.map(m => {
-        expect(m).to.be.greaterThan(55)
+      expect(m).to.be.greaterThan(55)
     })
-    expect(await nft.totalSupply()).to.be.equal(7)
-
-
-
+    expect(await nft.totalSupply()).to.be.equal(55+6+1)
   })
 
-  it("Test public mint", async function() {
-    await timeIncreaseTo(timestamp + 60*61)
+    it("Test public mint", async function() {
 
-    //tries to mint where public sale is not yet started
-    await expect(
-        nft.connect(signers[2]).mint()
-    ).to.be.revertedWith('PUBLIC_SALE_INACTIVE');
+      await nft.airdrop(signers[9].address, 55, { gasLimit: "7000000" });
 
-    await timeIncreaseTo(publicStart + 60)
+      await timeIncreaseTo(timestamp + 60*61)
 
-    let minted = []
-    await Promise.all(new Array(20).fill(0).map(async (a, i)=> {
-        let tx = await nft.connect(signers[i]).mint({gasLimit: "200000"})
-        tx = await tx.wait()
-        let tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
-        minted = [...minted, ...tokens]
-    }))
-    expect(minted.length).to.be.equal(20);
-    minted.map(m => {
-        expect(m).to.be.greaterThan(55)
+      //tries to mint where public sale is not yet started
+      await expect(
+          nft.connect(signers[2]).mint()
+      ).to.be.revertedWith('PUBLIC_SALE_INACTIVE');
+
+      await timeIncreaseTo(publicStart + 60)
+
+      let minted = []
+      await Promise.all(new Array(20).fill(0).map(async (a, i)=> {
+          let tx = await nft.connect(signers[i]).mint({gasLimit: "200000"})
+          tx = await tx.wait()
+          let tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
+          minted = [...minted, ...tokens]
+      }))
+      expect(minted.length).to.be.equal(20);
+      minted.map(m => {
+          expect(m).to.be.greaterThan(55)
+      })
+      expect(await nft.totalSupply()).to.be.equal(55+20)
+
+      let tx = await nft.connect(signers[1]).mint({gasLimit: "200000"})
+      tx = await tx.wait()
+      console.log("public mint", tx.gasUsed.toNumber())
+      let tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
+      minted = [...minted, ...tokens]
+
+      expect(await nft.totalSupply()).to.be.equal(55+21)
+
+      //tries to mint more than 2
+      await expect(
+          nft.connect(signers[1]).mint({gasLimit: "200000"})
+      ).to.be.revertedWith('PUBLIC_TOKEN_LIMIT');
+
+      //tries to mint where public sale ended
+      await timeIncreaseTo(publicEnd + 60)
+      await expect(
+          nft.connect(signers[1]).mint({gasLimit: "200000"})
+      ).to.be.revertedWith('PUBLIC_SALE_INACTIVE');
+
+      //override public sale
+      tx = await nft.overrideSales(true, false);
+      tx = await tx.wait()
+      await nft.connect(signers[7]).mint()
+      minted = [...minted, ...tokens]
+      expect(minted.length).to.be.equal(22);
+      minted.map(m => {
+          expect(m).to.be.greaterThan(55)
+      })
+      expect(await nft.totalSupply()).to.be.equal(55+22)
+
     })
-    expect(await nft.totalSupply()).to.be.equal(20)
 
-    let tx = await nft.connect(signers[1]).mint({gasLimit: "200000"})
-    tx = await tx.wait()
-    console.log("public mint", tx.gasUsed.toNumber())
-    let tokens = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())
-    minted = [...minted, ...tokens]
+    it("Test URI reveal", async function() {
 
-    expect(await nft.totalSupply()).to.be.equal(21)
+      await nft.airdrop(signers[9].address, 55, { gasLimit: "7000000" });
+      await timeIncreaseTo(publicStart + 60)
 
-    //tries to mint more than 2
-    await expect(
-        nft.connect(signers[1]).mint({gasLimit: "200000"})
-    ).to.be.revertedWith('PUBLIC_TOKEN_LIMIT');
+      let tx = await nft.connect(signers[1]).mint({gasLimit: "200000"})
+      tx = await tx.wait()
+      const token = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())[0]
+      const tempUri = await nft.tokenURI(token);
+      expect(tempUri).to.be.equal("www.tempuri.com/")
 
-    //tries to mint where public sale ended
-    await timeIncreaseTo(publicEnd + 60)
-    await expect(
-        nft.connect(signers[1]).mint({gasLimit: "200000"})
-    ).to.be.revertedWith('PUBLIC_SALE_INACTIVE');
+      await nft.setBaseURI("www.baseuri.com/");
+      await nft.toggleReveal();
+      const baseUri = await nft.tokenURI(token);
+      expect(baseUri).to.be.equal(`www.baseuri.com/${token}.json`)
 
-    //override public sale
-    tx = await nft.overrideSales(true, false);
-    tx = await tx.wait()
-    await nft.connect(signers[7]).mint()
-    minted = [...minted, ...tokens]
-    expect(minted.length).to.be.equal(22);
-    minted.map(m => {
-        expect(m).to.be.greaterThan(55)
     })
-    expect(await nft.totalSupply()).to.be.equal(22)
-
-  })
-
-  it("Test URI reveal", async function() {
-    await timeIncreaseTo(publicStart + 60)
-
-    let tx = await nft.connect(signers[1]).mint({gasLimit: "200000"})
-    tx = await tx.wait()
-    const token = tx.events.filter(e => e.event === "Transfer").map(e => e.args[2].toNumber())[0]
-    const tempUri = await nft.tokenURI(token);
-    expect(tempUri).to.be.equal("www.tempuri.com/")
-
-    await nft.setBaseURI("www.baseuri.com/");
-    await nft.toggleReveal();
-    const baseUri = await nft.tokenURI(token);
-    expect(baseUri).to.be.equal(`www.baseuri.com/${token}.json`)
-
-  })
 
 });
 
 
 function merkleTree(signers) {
-    let accounts = [];
-    for(let j = 1; j <= 10; j++) {
-        accounts.push(hashData(signers[j].address))
-    }
-    return new MerkleTree(accounts, utils.keccak256, { sortPairs: true });
-} 
+  let accounts = [];
+  for (let j = 1; j <= 10; j++) {
+    accounts.push(hashData(signers[j].address))
+  }
+  return new MerkleTree(accounts, utils.keccak256, { sortPairs: true });
+}
 
 const getProof = (merkleTree, account) => {
-    return merkleTree.getHexProof(hashData(account));
+  return merkleTree.getHexProof(hashData(account));
 }
 
 const hashData = (account) => {
-    return Buffer.from(ethers.utils.solidityKeccak256(['address'], [account]).slice(2), 'hex')
+  return Buffer.from(ethers.utils.solidityKeccak256(['address'], [account]).slice(2), 'hex')
 }
